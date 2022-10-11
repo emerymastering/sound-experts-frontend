f4d5201d33c120875ad0492dc71d56585a9f24c2
emerymastering <emery@mastering.lt>
2022-10-11 16:50:55 +0200

    deleteJob thunk added

--- src/store/jobs/thunks.js
+++ src/store/jobs/thunks.js
@@ -2,6 +2,8 @@ import { apiUrl } from "../../config/constants";
 import axios from "axios";
 import { startLoadingJobs, jobsFullyFetched, doneLoadingJobs } from "./slice";
 
+import { selectToken } from "../user/selectors";
+
 export const fetchJobs = () => async (dispatch, getState) => {
   try {
     dispatch(startLoadingJobs());
@@ -13,6 +15,23 @@ export const fetchJobs = () => async (dispatch, getState) => {
     dispatch(jobsFullyFetched({ jobs: response.data.jobs }));
 
     dispatch(doneLoadingJobs());
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const deleteJob = (id) => async (dispatch, getState) => {
+  try {
+    const token = selectToken(getState());
+    // console.log(token, "token");
+
+    // if we have no token, stop
+    if (token === null) return;
+    await axios.delete(`${apiUrl}/jobs/${id}`, {
+      // params: { id },
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    dispatch(fetchJobs());
   } catch (e) {
     console.log(e.message);
   }
