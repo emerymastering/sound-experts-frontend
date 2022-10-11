508af076c095f3d102602436c1148b9b5efa594e
emerymastering <emery@mastering.lt>
2022-10-11 16:47:46 +0200

    bacground setting commented

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "hero-pattern": "url('/img/hero-pattern.svg')",
-        "footer-texture": "url('/img/footer-texture.png')",
-      },
+      // backgroundImage: {
+      //   "hero-pattern": "url('/img/hero-pattern.svg')",
+      //   "footer-texture": "url('/img/footer-texture.png')",
+      // },
     },
   },
   plugins: [],
