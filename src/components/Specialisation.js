e462bf5839a98c48350e4c9a226adf0bd19bc2f6
emerymastering <emery@mastering.lt>
2022-10-13 14:15:04 +0200

    text edit

--- src/components/Specialisation.js
+++ src/components/Specialisation.js
@@ -8,7 +8,7 @@ export default function Specialisation(props) {
       className="bg-teal-500 hover:bg-teal-400 text-white font-bold py-10 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded min-h-full "
       style={{ color: "white" }}
     >
-      <p className="text-center">{props.title}</p>
+      <p className="text-center">{props.title}s</p>
     </div>
   );
 }
