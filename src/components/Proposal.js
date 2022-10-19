1510f1aa84b1c891277bf0a9036d7721bf00e7ad
emerymastering <emery@mastering.lt>
2022-10-19 17:10:31 +0200

    added accept proposal button

--- src/components/Proposal.js
+++ src/components/Proposal.js
@@ -2,9 +2,12 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
+import { acceptProposal } from "../store/jobs/thunks";
 
 export default function Proposal({ proposal }) {
   const date = DateTime.fromISO(proposal.createdAt).toLocaleString();
+  const dispatch = useDispatch();
+
   return (
     <div className="flex-col px-2 bg-sky-700 rounded">
       Proposal posted on {date}
@@ -32,12 +35,17 @@ export default function Proposal({ proposal }) {
             And my asking price is {proposal.price}€
           </div>
           <div className="w-1/3 pl-8">
-            <button
-              type="submit"
-              className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Accept proposal
-            </button>
+            {!proposal.accepted ? (
+              <button
+                type="submit"
+                className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={() => dispatch(acceptProposal(proposal.id))}
+              >
+                Accept proposal
+              </button>
+            ) : (
+              "Accepted"
+            )}
           </div>
         </div>
       </div>
