8afdba3bbbe20b2fdfb716f4de10a09b68c419d2
emerymastering <emery@mastering.lt>
2022-10-19 17:10:50 +0200

    styling

--- src/pages/Experts.js
+++ src/pages/Experts.js
@@ -15,25 +15,27 @@ export const Experts = () => {
   console.log("show me:", specialisations);
   return (
     <div className="h-screen bg-[url('../public/images/musicians.jpg')]  bg-contain bg-center ">
-      <h1 className="flex place-content-center text-3xl text-white p-10 ml-96 mr-96 bg-black bg-opacity-40">
-        Choose a category
-      </h1>
-      <div className="flex flex-row flex-wrap mx-32 justify-center">
-        {!specialisations.length
-          ? "Loading"
-          : specialisations.map((specialisation) => {
-              return (
-                <div
-                  className="p-10 w-60 h-50 bg-black bg-opacity-70 rounded-lg"
-                  key={specialisation.id}
-                >
-                  <Specialisation
-                    id={specialisation.id}
-                    title={specialisation.title}
-                  />
-                </div>
-              );
-            })}
+      <div className="pt-10">
+        <h1 className="flex place-content-center text-3xl text-white p-10 ml-96 mr-96 bg-black bg-opacity-40 rounded-lg">
+          Choose a category
+        </h1>
+        <div className="flex flex-row flex-wrap mx-32 justify-center pt-20">
+          {!specialisations.length
+            ? "Loading"
+            : specialisations.map((specialisation) => {
+                return (
+                  <div
+                    className="p-10 w-60 h-50 bg-black bg-opacity-70 rounded-lg"
+                    key={specialisation.id}
+                  >
+                    <Specialisation
+                      id={specialisation.id}
+                      title={specialisation.title}
+                    />
+                  </div>
+                );
+              })}
+        </div>
       </div>
     </div>
   );
