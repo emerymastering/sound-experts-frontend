4bd64f28aa0cda89ed27efcff20f1c36a49adef4
emerymastering <emery@mastering.lt>
2022-10-20 13:48:28 +0200

    disable navigate

--- src/components/Proposal.js
+++ src/components/Proposal.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
 import { acceptProposal } from "../store/jobs/thunks";
 
 export default function Proposal({ proposal }) {
   const date = DateTime.fromISO(proposal.createdAt).toLocaleString();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div className="flex-col px-2 bg-sky-700 rounded">
@@ -42,7 +42,7 @@ export default function Proposal({ proposal }) {
                 className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={() => {
                   dispatch(acceptProposal(proposal.id));
-                  navigate("/myaccount");
+                  // navigate("/myaccount");
                 }}
               >
                 Accept proposal
