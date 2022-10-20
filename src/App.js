71fded9e6798b306124e2ff328629176f260e063
emerymastering <emery@mastering.lt>
2022-10-20 16:21:09 +0200

    new routes fo experts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getUserWithStoredToken } from "./store/user/thunks";
+import { selectUser } from "./store/user/selectors";
 import { Routes, Route } from "react-router-dom";
 import { Navigation, MessageBox } from "./components";
 import {
@@ -11,13 +12,16 @@ import {
   Experts,
   JobForm,
   Jobs,
-  MyAccount,
   JobApply,
+  MyAccount,
+  MyAccountExpert,
   Proposals,
 } from "./pages";
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  console.log("user!!", user);
 
   useEffect(() => {
     dispatch(getUserWithStoredToken());
@@ -34,7 +38,10 @@ function App() {
         <Route path="/experts" element={<Experts />} />
         <Route path="/jobform" element={<JobForm />} />
         <Route path="/jobs" element={<Jobs />} />
-        <Route path="/myaccount" element={<MyAccount />} />
+        <Route
+          path="/myaccount"
+          element={user?.is_expert ? <MyAccountExpert /> : <MyAccount />}
+        />
         <Route path="/jobapply/:id" element={<JobApply />} />
         <Route path="/jobs/proposals/:job_id" element={<Proposals />} />
       </Routes>
