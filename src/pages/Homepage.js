adb7cd819bbe24b67c7b7b97b63e4be29ee14361
emerymastering <emery@mastering.lt>
2022-10-20 19:39:28 +0200

    styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Homepage = () => {
   return (
-    <div className="relative  min-h-screen max-h-screen bg-[url('../public/images/soundwaves.jpg')] bg-center bg-cover bg-no-repeat">
+    <div className="relative  min-h-screen max-h-screen bg-[url('../public/images/soundwaves.jpg')] bg-center bg-cover bg-no-repeat bg-fixed overflow-hidden">
       <h1 className="flex place-content-center text-3xl  p-10 text-gray-900 dark:text-blue-300 pb-10 text-center">
         Welcome to SoundExperts
       </h1>
@@ -11,8 +11,8 @@ export const Homepage = () => {
         Singers, Musicians, Producers, Mixing and Mastering Engineers will help
         you to take your production to the next level!
       </div>
-      <div className="flex lg:flex-nowrap sm:flex-wrap p-10 fixed top-80 left-10 right-10 bottom-20">
-        <div className="flex-auto w-1/2 p-20 pt-2 ml-2">
+      <div className="flex lg:flex-nowrap sm:flex-wrap p-10 fixed top-60 left-10 right-10 bottom-40">
+        <div className="flex-auto w-1/2 p-20 pt-20 mb-20 ml-4 mr-12">
           <Link to={"/experts"}>
             <button
               className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-20 px-4 border-b-4 border-gray-400 hover:border-gray-200 rounded-l-full w-full h-full text-xl opacity-95"
@@ -24,7 +24,7 @@ export const Homepage = () => {
             </button>
           </Link>
         </div>
-        <div className="flex-auto w-1/2 p-20 pt-2 mr-2 ">
+        <div className="flex-auto w-1/2 p-20 pt-20 mb-20 mr-4 ml-12">
           <Link to={"/jobs"}>
             <button
               className="bg-teal-700 hover:bg-teal-600 text-white font-bold py-20 px-4 border-b-4 border-teal-200 hover:border-teal-100 rounded-r-full w-full h-full text-xl opacity-95"
