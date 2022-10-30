37b18a695178005e60c8a1b301b63ec4a6e8f1df
emerymastering <emery@mastering.lt>
2022-10-30 16:47:58 +0100

    aded conditional rendering if no jobs

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -12,6 +12,7 @@ export const MyAccount = () => {
   const user = useSelector(selectUser);
 
   const jobs = useSelector(selectJobs);
+  console.log("any jobs?", jobs);
 
   const token = useSelector(selectToken);
 
@@ -23,7 +24,6 @@ export const MyAccount = () => {
     dispatch(fetchUserJobs());
   }, [dispatch]);
 
-  if (!jobs) return null;
   return (
     <div className="h-screen bg-[url('../public/images/factory.jpg')] bg-center bg-cover">
       <div className="h-screen bg-black bg-opacity-70 ">
@@ -32,34 +32,41 @@ export const MyAccount = () => {
             <div className="block text-2xl font-medium text-gray-900 dark:text-blue-300 pt-10 text-center">
               Hello {user.first_name}
             </div>
-            <p className="block text-xl font-medium text-gray-900 dark:text-blue-300 pt-10 pb-10 text-center">
-              Here is the list of your job offers
-            </p>
-            <div className="flex flex-col flex-wrap mx-32 justify-center">
-              {jobs.map((job) => {
-                return (
-                  <div
-                    className="p-5 m-auto
+            {jobs.length === 0 ? (
+              <p className="block text-xl font-medium text-gray-900 dark:text-blue-300 pt-10 pb-10 text-center">
+                You have not posted any jobs yet
+              </p>
+            ) : (
+              <div>
+                <p className="block text-xl font-medium text-gray-900 dark:text-blue-300 pt-10 pb-10 text-center">
+                  Here is the list of your job offers
+                </p>
+                <div className="flex flex-col flex-wrap mx-32 justify-center">
+                  {jobs.map((job) => {
+                    return (
+                      <div
+                        className="p-5 m-auto
                 "
-                    key={job.id}
-                  >
-                    <Job
-                      id={job.id}
-                      job={job}
-                      token={token}
-                      deleteEnabled={true}
-                      proposalsCount={job.job_applications.length}
-                      propEnable={
-                        token && !user?.is_expert && job.user_id === user.id
-                          ? true
-                          : false
-                      }
-                    />
-                  </div>
-                );
-              })}
-            </div>
-            <div className="flex flex-col flex-wrap mx-32 justify-center"></div>
+                        key={job.id}
+                      >
+                        <Job
+                          id={job.id}
+                          job={job}
+                          token={token}
+                          deleteEnabled={true}
+                          proposalsCount={job.job_applications.length}
+                          propEnable={
+                            token && !user?.is_expert && job.user_id === user.id
+                              ? true
+                              : false
+                          }
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
           </>
         )}
       </div>
