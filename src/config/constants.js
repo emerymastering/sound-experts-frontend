86db2955ff2bb919bb300e9be268bd98e2614d8d
emerymastering <emery@mastering.lt>
2022-11-11 16:09:18 +0100

    edit url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  "https://sound-experts-backend-production.up.railway.app/";
+export const apiUrl = "https://sound-experts-backend-production.up.railway.app";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
